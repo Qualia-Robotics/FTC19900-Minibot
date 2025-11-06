eeb109b8aedf3900c99c1dc1e1a1bfcce7f996a8
spati <ftcvelocity@gmail.com>
2025-11-05 19:08:02 -0600

    fixed strafe left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Amulya.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Amulya.java
@@ -129,10 +129,10 @@ public class Auto_Amulya extends LinearOpMode {
 
     private void strafeLeft(double inches) {
         int move = (int)(inches * COUNTS_PER_INCH);
-        frontleft.setTargetPosition(frontleft.getCurrentPosition() + move);
-        frontright.setTargetPosition(frontright.getCurrentPosition() - move);
-        backleft.setTargetPosition(backleft.getCurrentPosition() + move);
-        backright.setTargetPosition(backright.getCurrentPosition() - move);
+        frontleft.setTargetPosition(frontleft.getCurrentPosition() - move);
+        frontright.setTargetPosition(frontright.getCurrentPosition() + move);
+        backleft.setTargetPosition(backleft.getCurrentPosition() - move);
+        backright.setTargetPosition(backright.getCurrentPosition() + move);
 
         runToPosition();
     }
