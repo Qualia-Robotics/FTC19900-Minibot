a9663a6c5b02e7d770eb112b483fd97c727524d6
EdwardCasler <edwardcasler@hotmail.com>
2025-11-07 17:18:21 -0800

    first comminit
    11/7/2025

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp
+public class MotorTest extends LinearOpMode {
+    private DcMotor rightDrive;
+
+    @Override
+    public void runOpMode() {
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                rightDrive.setPower(0.2);
+            }
+        }
+    }
+}
