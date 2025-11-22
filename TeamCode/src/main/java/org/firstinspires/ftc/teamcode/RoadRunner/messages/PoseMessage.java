b82aa2332b684fc58431ee495dd4d054af586a9c
EagleNotBot <phibersto26@gmail.com>
2025-11-21 20:34:53 -0600

    Dealt with what little errors I could
    Roadrunner still isn't imported, that should clear up most of the errors in the remaining 7 classes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/messages/PoseMessage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/messages/PoseMessage.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.RoadRunner.messages;
 
-import com.acmerobotics.roadrunner.Pose2d;
+import org.firstinspires.ftc.teamcode.Swerve.wpilib.geometry.Pose2d;
 
 public final class PoseMessage {
     public long timestamp;
@@ -10,9 +10,9 @@ public final class PoseMessage {
 
     public PoseMessage(Pose2d pose) {
         this.timestamp = System.nanoTime();
-        this.x = pose.position.x;
-        this.y = pose.position.y;
-        this.heading = pose.heading.toDouble();
+        this.x = pose.getX();
+        this.y = pose.getY();
+        this.heading = pose.getRotation().getDegrees();
     }
 }
 
