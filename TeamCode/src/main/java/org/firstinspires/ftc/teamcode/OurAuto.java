5ca2a3502118d426063a4ee0f2370ba8bc1f1ad7
EdwardCasler <edwardcasler@hotmail.com>
2025-11-21 17:23:17 -0800

    removed moving from auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurAuto.java
@@ -8,12 +8,13 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
+
 @Autonomous
 public class OurAuto extends LinearOpMode {
+
+
     private DcMotorEx flywheel;
     private DcMotor feedRoller;
-    private DcMotorEx leftDrive;
-    private DcMotorEx rightDrive;
     private CRServo agitator;
     float ticksPerRev = 28 * 5 * 3; //5 and 3 is the gear reductions
     float circumference = 9.5f; //in inches
@@ -37,39 +38,12 @@ public class OurAuto extends LinearOpMode {
         flywheel = hardwareMap.get(DcMotorEx.class, "flywheel");
         feedRoller = hardwareMap.get(DcMotor.class, "coreHex");
         agitator = hardwareMap.get(CRServo.class, "servo");
-        leftDrive = hardwareMap.get(DcMotorEx.class, "leftDrive");
-        rightDrive = hardwareMap.get(DcMotorEx.class, "rightDrive");
-
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         flywheel.setDirection(DcMotorSimple.Direction.REVERSE);
         feedRoller.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         while(opModeIsActive()) {
-            leftDrive.setTargetPosition((int)(ticksPerInch * 23));
-            rightDrive.setTargetPosition((int)(ticksPerInch * 23));
-
-            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            leftDrive.setPower(0.2);
-            rightDrive.setPower(0.2);
-
-            while(leftDrive.isBusy() || rightDrive.isBusy()) {
-                telemetry.addLine(String.valueOf(leftDrive.getCurrentPosition()));
-                telemetry.addLine(String.valueOf(rightDrive.getCurrentPosition()));
-                telemetry.update();
-            }
-
-            leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
             double multiplier = 14 / getLowestVoltage();
             flywheel.setVelocity(1300 * multiplier);
             sleep(1000);
