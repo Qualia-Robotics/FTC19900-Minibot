156d77bbbf29f1b18ee7fe5be9248deecdee2d66
Your Name <aadikulkarni9@gmail.com>
2025-11-25 19:34:26 -0500

    Add Pinpoint odometry localizer configuration to Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,16 +3,30 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
+    // Pinpoint localizer - add this first, then configure in Robot Configuration
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(0)  // We'll measure this after testing
+            .strafePodX(0)   // We'll measure this after testing
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")  // This name must match what you configure in Robot Configuration
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)  // This connects Pinpoint to Pedro Pathing
                 .pathConstraints(pathConstraints)
                 .build();
     }
