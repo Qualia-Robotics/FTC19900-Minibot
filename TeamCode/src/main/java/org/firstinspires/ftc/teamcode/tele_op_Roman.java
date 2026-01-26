d242dc27fdfd3f851211932da149443574d9db9e
robotel369 <roboromatel2@gmail.com>
2026-01-26 00:33:01 -0800

    Update tele_op_Roman.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele_op_Roman.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele_op_Roman.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@Autonomous
+@TeleOp
 public class tele_op_Roman extends OpMode {
 
     @Override
